46fc5a516cd809a22c5ff72bd05975f20959d2d5
anuanaghavm <anaghavm2019@gmail.com>
2025-07-01 11:11:34 +0530

    ..

--- crmtel/settings.py
+++ crmtel/settings.py
@@ -98,12 +98,12 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
@@ -115,6 +115,18 @@ DATABASES = {
 #         'PORT': '3306',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crm_telecaller',
+        'USER': 'crm_telecaller_user',
+        'PASSWORD': 'fm3QuolkwiXnpP3zIbRyNAvopncyDMTT',
+        'HOST': 'dpg-d1hmucruibrs73fgrpl0-a.oregon-postgres.render.com',  # Replace with your full host
+        'PORT': '5432',
+    }
+}
+
 
 
 # Password validation
