f62994e11c205e7b26b4dd9976723e8af4664976
theertha09 <161118217+theertha09@users.noreply.github.com>
2025-07-01 10:35:41 +0530

    /

--- callregister/models.py
+++ callregister/models.py
@@ -28,6 +28,7 @@ class CallRegister(models.Model):
         ('Converted', 'Converted'),
         ('Do Not Call', 'Do Not Call'),
         ('walk_in_list', 'walk_in_list'),
+        ('not_contacted', 'not_contacted'),
     ]
 
     enquiry = models.ForeignKey(Enquiry, on_delete=models.CASCADE, related_name='call_logs')
